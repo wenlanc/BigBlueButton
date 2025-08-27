38a5d696537c4c00468f9bd9d65a44b26925ae44
Father Vlasie <33334298+fvlasie@users.noreply.github.com>
2025-08-27 16:57:29 -0700

    s

--- manifest.php
+++ manifest.php
@@ -2,7 +2,7 @@
 /*
 Gibbon, Flexible & Open School System
 Copyright (C) 2022, Father Vlasie
-
+ 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
