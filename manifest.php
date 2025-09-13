8b9147a1d9618702cd1c7654e50086f35faa89d3
fvlasie <33334298+fvlasie@users.noreply.github.com>
2025-09-12 23:12:41 -0700

    Update manifest.php

--- manifest.php
+++ manifest.php
@@ -25,7 +25,7 @@ $description = 'A module to support BigBlueButton Integration.';            // S
 $entryURL = "bbb_settings.php";
 $type = "Additional";
 $category = "Admin";
-$version = "1.0.1";
+$version = "1.0.2";
 $author = "Wenlan Cui";
 $url = "";
 
